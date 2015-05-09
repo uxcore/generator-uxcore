4f59e7c326e5e90027b9d415e4067ecc9e26ba85
骁勇 <zhouquan.yezq@alibaba-inc.com>
2015-05-10 07:49:13 +0800

    add generator-uxcore

---
+++ generators/app/index.js
@@ -0,0 +1,57 @@
+var yeoman = require('yeoman-generator');
+var Base = yeoman.generators.Base;
+var Path = require('path');
+var fs = require('fs');
+
+function camelCase(name){
+  return name.replace(/-\w/g,function(m){
+    return m.charAt(1).toUpperCase();
+  })
+}
+
+module.exports = Base.extend({
+  constructor: function () {
+    Base.apply(this, arguments);
+    this.option('port', {type: Number, defaults: '8000'});
+    this.option('uxcore_version', {type: String, defaults: '1.0.0'});
+    this.option('author', {type: String, defaults: ''});
+  },
+
+  welcome: function () {
+    this.appname = this.appname.replace(/\s/g, '-');
+    this.AppName = this.appname.charAt(0).toUpperCase() + camelCase(this.appname.slice(1));
+    this.log('welcome to generator-uxcore: ' + this.appname);
+    this.port = this.options.port;
+    this.version = this.options.rc_version;
+    this.author = this.options.author;
+  },
+
+  setup: function () {
+    var self = this;
+    this.src.recurse('.', function (path, root, subdir, filename) {
+      var oldPath = path;
+      // https://github.com/npm/npm/issues/1862
+      if (filename === 'gitignore') {
+        filename = '.gitignore';
+        var newPath = Path.join(Path.dirname(path), filename);
+        fs.renameSync(path, newPath);
+        path = newPath;
+      }
+      self.template(path, subdir ? Path.resolve(subdir, filename) : filename);
+      if (filename === '.gitignore') {
+        fs.renameSync(path, oldPath);
+      }
+    });
+    var AppName = this.AppName;
+    this.dest.write('lib/' + AppName + '.js', [
+      '/** @jsx React.DOM */',
+      'var React = require(\'react\');',
+      'var ' + AppName + ' = React.createClass();',
+      'module.exports = ' + AppName + ';'
+    ].join('\n'));
+  },
+
+  done: function () {
+    this.log('done');
+  }
+});
---
+++ generators/app/templates/examples/simple.js
@@ -0,0 +1,6 @@
+/** @jsx React.DOM */
+// use jsx to render html, do not modify simple.html
+require('uxcore-<%= appname%>/style/index.css');
+var <%= AppName%> = require('uxcore-<%= appname%>');
+var React = require('react');
+React.render(<<%= AppName%> />, document.getElementById('__react-content'));
