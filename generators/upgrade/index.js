01ac7984c056a40702d195e75af3ed8665ac3919
光弘 <guanghong.wsj@alibaba-inc.com>
2015-11-24 19:43:10 +0800

    update dependency object-assign to ^4.0.0

--- generators/upgrade/index.js
+++ generators/upgrade/index.js
@@ -2,11 +2,13 @@
 var path = require('path');
 var _ = require('lodash');
 var yeoman = require('yeoman-generator');
+var file = require('html-wiring');
 
 module.exports = yeoman.generators.Base.extend({
     inherit: function() {
         var me = this;
-        me.bulkCopy('gulpfile.js', 'gulpfile.js');
-        me.bulkCopy('webpack.dev.js', 'webpack.dev.js');
+        var pkg = JSON.parse(file.readFileAsString('package.json'));
+        pkg.dependencies['object-assign'] = '^4.0.0';
+        file.writeFileFromString(JSON.stringify(pkg, null, '  '), 'package.json');
     }
 });
